4d18256820aac0961c3224631cf5eeea6dffb3e4
wraithio <aarmon5@gmail.com>
2025-03-11 16:01:23 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import TipComponent from "./components/TipComponent";
+
+function App() {
+
+  return (
+    <TipComponent/>
+  );
+}
+
+export default App;
---
+++ src/components/TipComponent.jsx
@@ -0,0 +1,241 @@
+import { useState, useEffect } from "react";
+
+const TipComponent = () => {
+  const [bill, setBill] = useState(0);
+  const [people, setPeople] = useState(null);
+  const [tip, setTip] = useState("0.00");
+  const [total, setTotal] = useState("0.00");
+
+  const grabBill = async () => {
+    setBill(billInput.value);
+  };
+
+  useEffect(() => {
+    resetBtn.setAttribute("disabled", "");
+    resetBtn.style.opacity = "0.4";
+    resetBtn.classList.remove("hover:bg-[#9fe8df]");
+    localStorage.setItem("TipPercent","")
+  }, []);
+  useEffect(() => {
+    // console.log(`bill:${bill}`)
+    if (localStorage.getItem("TipPercent", 0) != "") {
+      // console.log(localStorage.getItem("TipPercent", 0))
+      generateResult(localStorage.getItem("TipPercent", 0), null);
+    }
+  }, [bill]);
+
+  const grabPeople = async () => {
+    setPeople(peopleInput.value);
+  };
+
+  useEffect(() => {
+    // console.log(tip);
+    // console.log(`people:${people}`)
+    warningText.className = "hidden"
+    validText.className = "dark-cyan mb-2"
+    peopleInput.style.border = "none"
+
+    if (localStorage.getItem("TipPercent", 0) != "" || people != 0) {
+      generateResult(localStorage.getItem("TipPercent", 0), null);
+    }
+
+    if(people == 0)
+    {
+        peopleInput.style.border = "1px solid red"
+        warningText.className = "show"
+        validText.className = "dark-cyan mb-2 hidden"
+    }
+  }, [people]);
+
+  function roundToHundredth(number) {
+    const rounded = Math.round(number * 100) / 100;
+    return rounded.toFixed(2);
+  }
+
+  //switches custom button to an input field
+  const toggleCustom = async () => {
+    customBtn.classList.toggle("hidden");
+    customInput.classList.toggle("hidden");
+    const TipBtns = [TipBtn5, TipBtn10, TipBtn15, TipBtn25, TipBtn50];
+    for (let i = 0; i < TipBtns.length; i++) {
+      TipBtns[i].className =
+        "text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]";
+    }
+  };
+
+  //finds tip amounts and cost per person
+  const generateResult = async (tip, btn) => {
+    //reset button activiation and tip decimal formatting
+    resetBtn.removeAttribute("disabled");
+    resetBtn.style.opacity = "1";
+    resetBtn.classList.add("hover:bg-[#9fe8df]");
+    if (tip[0] != ".") {
+      if (tip.length == 1) {
+        tip = ".0" + tip;
+      } else {
+        tip = "." + tip;
+      }
+    }
+    localStorage.setItem("TipPercent", tip);
+    console.log(`total bill:${bill}, people:${people}, tip%${tip}`);
+    let tipAmount = (bill * tip) / people;
+    let tipTotal = bill / people + tipAmount;
+    setTip(roundToHundredth(tipAmount));
+    setTotal(roundToHundredth(tipTotal));
+    const TipBtns = [TipBtn5, TipBtn10, TipBtn15, TipBtn25, TipBtn50];
+    if (btn != null) {
+      for (let i = 0; i < TipBtns.length; i++) {
+        TipBtns[i].className =
+          "text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]";
+      }
+      btn.className = "rounded-[.25rem] text-[#00494d] bg-[#26c2ad]";
+
+      if (customBtn.classList.contains("hidden")) {
+        toggleCustom();
+      }
+    } else {
+      for (let i = 0; i < TipBtns.length; i++) {
+        TipBtns[i].className =
+          "text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]";
+      }
+    }
+  };
+
+  const clearFields = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center place-items-center">
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-center">
+          <img src="/images/logo.svg" alt="tip calculator logo" />
+        </div>
+        <div className="flex flex-row gap-10 bg-white py-6 px-8 rounded-xl max-w-25 drop-shadow-xl">
+          <div className="flex flex-col gap-4">
+            <div>
+              <h3 className="dark-cyan mb-2">Bill</h3>
+              <input
+                type="number"
+                id="billInput"
+                onChange={grabBill}
+                placeholder="0"
+                min="0"              />
+            </div>
+            <div>
+              <h3 className="dark-cyan mb-2">Select Tip %</h3>
+              <div className="grid grid-rows-2 grid-cols-3 gap-2">
+                <button
+                  id="TipBtn5"
+                  className="text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]"
+                  onClick={() => generateResult(5, TipBtn5)}
+                >
+                  <h2 className="text-lg py-1">5%</h2>
+                </button>
+                <button
+                  id="TipBtn10"
+                  className="text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]"
+                  onClick={() => generateResult(1, TipBtn10)}
+                >
+                  <h2 className="text-lg py-1">10%</h2>
+                </button>
+                <button
+                  id="TipBtn15"
+                  className="text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]"
+                  onClick={() => generateResult(15, TipBtn15)}
+                >
+                  <h2 className="text-lg py-1">15%</h2>
+                </button>
+                <button
+                  id="TipBtn25"
+                  className="text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]"
+                  onClick={() => generateResult(25, TipBtn25)}
+                >
+                  <h2 className="text-lg py-1">25%</h2>
+                </button>
+                <button
+                  id="TipBtn50"
+                  className="text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]"
+                  onClick={() => generateResult(5, TipBtn50)}
+                >
+                  <h2 className="text-lg py-1">50%</h2>
+                </button>
+                <button
+                  className="darkish-cyan bg-[#f2f7f7] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df] text-sm tracking-widest"
+                  id="customBtn"
+                  onClick={toggleCustom}
+                >
+                  <b className="text-lg">Custom</b>
+                </button>
+
+                <input
+                  type="number"
+                  className="hidden w-24 rounded-[.25rem]"
+                  placeholder="%"
+                  id="customInput"
+                  onChange={() => generateResult(customInput.value, null)}
+                  min="0"   
+                />
+              </div>
+            </div>
+            <div>
+              <h3 className="dark-cyan mb-2" id="validText">Number of People</h3>
+              <div id="warningText">
+              <div className="flex justify-between ">
+              <h3 className="dark-cyan mb-2">Number of People</h3>
+              <h3 className="text-red-500 mb-2"><b>Can't be zero</b></h3>
+              </div>
+              </div>
+              <input
+                type="number"
+                id="peopleInput"
+                placeholder="0"
+                onChange={grabPeople}
+              />
+            </div>
+          </div>
+          <div className="bg-[#00494d] rounded-lg flex flex-col gap-4 px-8 pb-6 pt-8">
+            <div className="flex gap-8 justify-between">
+              <div>
+                <h3 className="text-white">Tip Amount</h3>
+                <h3 className="gray-cyan">/ person</h3>
+              </div>
+              <div className="flex place-items-center text-[#26c0ab] text-4xl">
+                <h1>
+                  <b>${tip}</b>
+                </h1>
+              </div>
+            </div>
+            <div className="flex gap-8 justify-between">
+              <div>
+                <h3 className="text-white">Total</h3>
+                <h3 className="gray-cyan">/ person</h3>
+              </div>
+              <div className="flex place-items-center text-[#26c0ab] text-4xl">
+                <h1>
+                  <b>${total}</b>
+                </h1>
+              </div>
+            </div>
+            <div className="h-full flex place-items-end justify-center">
+              <button
+                className="flex w-full rounded-md py-1 bg-[#26c0ab] hover:bg-[#9fe8df]"
+                type="reset"
+                id="resetBtn"
+                onClick={clearFields}
+              >
+                <div className="flex w-full justify-center">
+                  <h1 className="dark-cyan text-lg">
+                    <b>RESET</b>
+                  </h1>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TipComponent;
