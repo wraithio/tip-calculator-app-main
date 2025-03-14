4c0cecbc847b41f3fa7bdea46c7a13d223150860
wraithio <aarmon5@gmail.com>
2025-03-13 22:35:07 -0700

    responsiveness

--- src/components/TipComponent.jsx
+++ src/components/TipComponent.jsx
@@ -35,7 +35,8 @@ const TipComponent = () => {
     validText.className = "dark-cyan mb-2"
     peopleInput.style.border = "none"
 
-    if (localStorage.getItem("TipPercent", 0) != "" || people != 0) {
+    if (localStorage.getItem("TipPercent", 0) != "") {
+      console.log("1")
       generateResult(localStorage.getItem("TipPercent", 0), null);
     }
 
@@ -69,7 +70,7 @@ const TipComponent = () => {
     resetBtn.removeAttribute("disabled");
     resetBtn.style.opacity = "1";
     resetBtn.classList.add("hover:bg-[#9fe8df]");
-    if (tip[0] != ".") {
+    if (tip != ".") {
       if (tip.length == 1) {
         tip = ".0" + tip;
       } else {
@@ -108,10 +109,10 @@ const TipComponent = () => {
   return (
     <div className="min-h-screen flex justify-center place-items-center">
       <div className="flex flex-col gap-8">
-        <div className="flex justify-center">
+        <div className="flex justify-center sm:mt-0 mt-8">
           <img src="/images/logo.svg" alt="tip calculator logo" />
         </div>
-        <div className="flex flex-row gap-10 bg-white py-6 px-8 rounded-xl max-w-25 drop-shadow-xl">
+        <div className="flex sm:flex-row flex-col sm:w-fit w-screen sm:h-fit h-[37rem] gap-10 bg-white py-6 px-8 sm:rounded-xl rounded-t-3xl drop-shadow-xl">
           <div className="flex flex-col gap-4">
             <div>
               <h3 className="dark-cyan mb-2">Bill</h3>
@@ -120,45 +121,46 @@ const TipComponent = () => {
                 id="billInput"
                 onChange={grabBill}
                 placeholder="0"
-                min="0"              />
+                min="0"
+                className="sm:w-fit w-full"              />
             </div>
             <div>
               <h3 className="dark-cyan mb-2">Select Tip %</h3>
-              <div className="grid grid-rows-2 grid-cols-3 gap-2">
+              <div className="grid sm:grid-rows-2 sm:grid-cols-3 grid-cols-2 grid-rows-3 gap-2">
                 <button
                   id="TipBtn5"
                   className="text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]"
                   onClick={() => generateResult(5, TipBtn5)}
                 >
-                  <h2 className="text-lg py-1">5%</h2>
+                  <h2 className="sm:text-lg text-xl py-1">5%</h2>
                 </button>
                 <button
                   id="TipBtn10"
                   className="text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]"
                   onClick={() => generateResult(1, TipBtn10)}
                 >
-                  <h2 className="text-lg py-1">10%</h2>
+                  <h2 className="sm:text-lg text-xl py-1">10%</h2>
                 </button>
                 <button
                   id="TipBtn15"
                   className="text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]"
                   onClick={() => generateResult(15, TipBtn15)}
                 >
-                  <h2 className="text-lg py-1">15%</h2>
+                  <h2 className="sm:text-lg text-xl py-1">15%</h2>
                 </button>
                 <button
                   id="TipBtn25"
                   className="text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]"
                   onClick={() => generateResult(25, TipBtn25)}
                 >
-                  <h2 className="text-lg py-1">25%</h2>
+                  <h2 className="sm:text-lg text-xl py-1">25%</h2>
                 </button>
                 <button
                   id="TipBtn50"
                   className="text-white bg-[#00494d] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df]"
                   onClick={() => generateResult(5, TipBtn50)}
                 >
-                  <h2 className="text-lg py-1">50%</h2>
+                  <h2 className="sm:text-lg text-xl py-1">50%</h2>
                 </button>
                 <button
                   className="darkish-cyan bg-[#f2f7f7] rounded-[.25rem] hover:text-[#00494d] hover:bg-[#9fe8df] text-sm tracking-widest"
@@ -174,7 +176,7 @@ const TipComponent = () => {
                   placeholder="%"
                   id="customInput"
                   onChange={() => generateResult(customInput.value, null)}
-                  min="0"   
+                  min="0"  
                 />
               </div>
             </div>
@@ -191,27 +193,28 @@ const TipComponent = () => {
                 id="peopleInput"
                 placeholder="0"
                 onChange={grabPeople}
+                className="sm:w-fit w-full" 
               />
             </div>
           </div>
-          <div className="bg-[#00494d] rounded-lg flex flex-col gap-4 px-8 pb-6 pt-8">
-            <div className="flex gap-8 justify-between">
+          <div className="bg-[#00494d] rounded-lg flex flex-col gap-4 sm:px-8 sm:pb-6 sm:pt-8 p-4 pt-6">
+            <div className="flex sm:gap-8 gap-4 justify-between">
               <div>
-                <h3 className="text-white">Tip Amount</h3>
-                <h3 className="gray-cyan">/ person</h3>
+                <h3 className="text-white sm:text-base text-xs">Tip Amount</h3>
+                <h3 className="gray-cyan sm:text-base text-xs">/ person</h3>
               </div>
-              <div className="flex place-items-center text-[#26c0ab] text-4xl">
+              <div className="flex place-items-center text-[#26c0ab] text-2xl sm:text-4xl">
                 <h1>
                   <b>${tip}</b>
                 </h1>
               </div>
             </div>
-            <div className="flex gap-8 justify-between">
+            <div className="flex sm:gap-8 gap-4 justify-between">
               <div>
-                <h3 className="text-white">Total</h3>
-                <h3 className="gray-cyan">/ person</h3>
+                <h3 className="text-white sm:text-base text-xs">Total</h3>
+                <h3 className="gray-cyan sm:text-base text-xs">/ person</h3>
               </div>
-              <div className="flex place-items-center text-[#26c0ab] text-4xl">
+              <div className="flex place-items-center text-[#26c0ab] text-2xl sm:text-4xl">
                 <h1>
                   <b>${total}</b>
                 </h1>
